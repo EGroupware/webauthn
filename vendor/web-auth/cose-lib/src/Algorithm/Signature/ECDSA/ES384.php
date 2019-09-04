ed10b3c66676aa1285b8ec79cda559c0746f8808
Ralf Becker <RalfBecker@outdoor-training.de>
2019-09-04 18:12:24 +0200

    commit vendor directory for easier installation

---
+++ vendor/web-auth/cose-lib/src/Algorithm/Signature/ECDSA/ES384.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2019 Spomky-Labs
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+namespace Cose\Algorithm\Signature\ECDSA;
+
+use Cose\Key\Ec2Key;
+use Cose\Key\Key;
+
+final class ES384 extends ECDSA
+{
+    public const ID = -35;
+
+    public static function identifier(): int
+    {
+        return self::ID;
+    }
+
+    public function sign(string $data, Key $key): string
+    {
+        $signature = parent::sign($data, $key);
+
+        return ECSignature::fromAsn1($signature, $this->getSignaturePartLength());
+    }
+
+    public function verify(string $data, Key $key, string $signature): bool
+    {
+        $signature = ECSignature::toAsn1($signature, $this->getSignaturePartLength());
+
+        return parent::verify($data, $key, $signature);
+    }
+
+    protected function getHashAlgorithm(): int
+    {
+        return OPENSSL_ALGO_SHA384;
+    }
+
+    protected function getCurve(): int
+    {
+        return Ec2Key::CURVE_P384;
+    }
+
+    protected function getSignaturePartLength(): int
+    {
+        return 96;
+    }
+}
