d1f0dfbe0a3d288bd001db0a6ae83feff2c363f8
Ralf Becker <RalfBecker@outdoor-training.de>
2020-06-28 10:13:55 +0200

    bump version to 20.1

--- setup/tables_current.inc.php
+++ setup/tables_current.inc.php
@@ -1,13 +1,12 @@
 <?php
 /**
- * eGroupWare - Setup
- * http://www.egroupware.org
+ * EGroupware - Setup
+ * https://www.egroupware.org
  * Created by eTemplates DB-Tools written by ralfbecker@outdoor-training.de
  *
  * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
  * @package webauthn
  * @subpackage setup
- * @version $Id$
  */
 
 
